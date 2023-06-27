b188248bb075a9e790ccfbe9eb07375fa85a444c
양회진 <yanghoejin@yanghoejin-ui-MacBookAir.local>
2023-06-27 23:33:55 +0900

    ADD : login layout

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main';
+// import Main from './pages/Main';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
-import '/data.json';
+import Login from './pages/Login';
+// import '/data.json';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,78 @@
+// import { Link } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import './Login.scss';
 
 const Login = () => {
+  const [inputValue, setInputValue] = useState({});
+  const [btnColor, setBtnColor] = useState(true);
+  const [inputClass, setInputclass] = useState(true);
+
+  const conditon =
+    inputValue?.id?.indexOf('@') !== -1 && inputValue?.pw?.length >= 5;
+
+  // function goToMain() {
+  //   navigate('/MainHoejin');
+  // }
+
+  const handleInput = e => {
+    const { name, value } = e.target;
+    setInputValue({ ...inputValue, [name]: value });
+  };
+
+  const changeBtnColor = () => {
+    setBtnColor(!btnColor);
+  };
+
   return (
     <div className="login">
       <p className="title">로그인</p>
       <div className="inputContainer">
-        <input className="id" placeholder="아이디" />
-        <p>! 아이디를 입력해주세요.</p>
-        <input className="pw" placeholder="비밀번호" />
-        <p>! 비밀번호를 입력해주세요.</p>
+        <label className="idLabel">아이디</label>
+        <input
+          name="id"
+          className={`id ${inputValue.id?.length > 0 ? 'idInputLabel' : ''}`}
+          onClick={() => setInputclass(!inputClass)}
+          onChange={e => {
+            handleInput(e);
+          }}
+        />
+        <p className="hidden red">! 아이디를 입력해주세요.</p>
+        <label className="pwLabel">비밀번호</label>
+        <input
+          name="pw"
+          className="pw"
+          type="password"
+          onChange={e => {
+            handleInput(e);
+          }}
+        />
+        <p className="hidden red">! 비밀번호를 입력해주세요.</p>
         <div className="saveContainer">
           <input className="checkbox" type="checkbox" />
           <p className="saveId">아이디 저장</p>
         </div>
       </div>
-      <button className="loginButton">로그인</button>
+      <button
+        className="loginButton"
+        disabled={conditon ? false : true}
+        onChange={changeBtnColor}
+        // onClick={goToMain}
+      >
+        로그인
+      </button>
       <div className="buttonContainer">
+        {/* <Link to={"./signIn.js"}> */}
         <button className="admition">회원가입</button>
+        {/* </Link> */}
         <p>|</p>
+        {/* <Link to={"./signIn.js"}> */}
         <button className="findId">아이디 찾기</button>
+        {/* </Link> */}
         <p>|</p>
+        {/* <Link to={"./signIn.js"}> */}
         <button className="findPw">비밀번호 찾기</button>
+        {/* </Link> */}
       </div>
     </div>
   );
