19be75599019e12652230c798c2d94ab9e702839
이지원 <manz19@ijiwon-ui-MacBookAir.local>
2023-06-27 20:54:54 +0900

    ADD : Nav/layout 구현

--- src/components/Footer/Footer.js
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <div>df</div>;
-};
-
-export default Footer;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import './Nav.scss';
+
 const Nav = () => {
-  return <div>df</div>;
+  return (
+    <div className="nav">
+      <div className="navContainer">
+        <span className="mainLogo">배민배민배</span>
+        <div className="navMenu">
+          <div className="link">
+            <Link to="">
+              <button className="menuBtn">전체</button>
+            </Link>
+            <Link to="">
+              <button className="menuBtn">문구</button>
+            </Link>
+            <Link to="">
+              <button className="menuBtn">리빙</button>
+            </Link>
+            <Link to="">
+              <button className="menuBtn">책/매거진F</button>
+            </Link>
+            <Link to="">
+              <button className="menuBtn">콜라보레이션</button>
+            </Link>
+            <Link to="">
+              <button className="menuBtn">명예의 전당</button>
+            </Link>
+          </div>
+          <div className="icons">
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className="leadingGlass"
+            />
+            <FontAwesomeIcon icon={faCartShopping} className="cart" />
+            <button className="loginBtn">로그인</button>
+            <FontAwesomeIcon icon={faBars} className="bar" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
