cffce8224537c258e5856a54ce9c2228b37bcb32
양회진 <yanghoejin@yanghoejin-ui-MacBookAir.local>
2023-06-28 16:33:09 +0900

    conflict

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Main from './pages/Main';
+import Main from './pages/Main';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import Login from './pages/Login';
-// import '/data.json';
+import '/data.json';
 
 const Router = () => {
   return (
