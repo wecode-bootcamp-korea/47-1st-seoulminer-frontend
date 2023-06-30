880c0b7d0f33ecfe3e79dd72fd9ee928bb8e8697
양회진 <yanghoejin@yanghoejin-ui-MacBookAir.local>
2023-07-01 00:32:30 +0900

    ADD : 404 page

--- src/Router.js
+++ src/Router.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import Main from './pages/Main';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import Login from './pages/Login';
 import Products from './pages/Products';
+import Error from './pages/Error';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Main />} />
+          <Route path="products" element={<Products />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+        <Route path="*" element={<Error />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
 
 export default Router;
+
+const Layout = () => {
+  return (
+    <>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
