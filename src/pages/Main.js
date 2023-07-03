2df85277f44c1e9c47ebfd47c86e9aec598a8522
이지원 <manz19@ijiwon-ui-MacBookAir.local>
2023-07-03 10:59:34 +0900

    ADD : 메인페이지 완료

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import MainSlide from './MainSlide';
