e081f4710227e01e4be0c1bf3bc7a3b981b35861
이지원 <manz19@ijiwon-ui-MacBookAir.local>
2023-07-04 18:28:30 +0900

    ADD : cart-layout

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,27 +3,39 @@ import './Cart.scss';
 
 const Cart = () => {
   return (
-    <div className="container">
-      <div className="firstTextBorder">
-        <p className="firstText">장바구니</p>
+    <div className="cartContainer">
+      <div className="orderStep">
+        <p className="cartText">장바구니</p>
       </div>
-      <div className="text">
-        <p className="secondText">앗!</p>
-        <p className="thirdText">장바구니가 텅~~</p>
+      <div className="cartWrapper">
+        <div className="cartContent">
+          <div className="cartHeader">
+            <div className="dataNone">
+              <h2 className="title">앗!</h2>
+              <div className="text">장바구니가 텅~</div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="paymentArea">
-        <div className="paymentList">
-          <p>총 상품금액</p>
-          <p>배송비</p>
-          <p>0원</p>
-          <p>+0원</p>
-        </div>
-        <div className="paymentResult">
-          <p>결제예상금액</p>
-          <p>0원</p>
-        </div>
-        <div className="paymentButton">
-          <button className="payBtn">0원 주문하기</button>
+        <div className="paymentBorder">
+          <div className="paymentResult">
+            <p className="price">총 상품금액</p>
+            <p className="priceNum">0원</p>
+            <div className="deliveryPrice">
+              <p className="priceText">배송비</p>
+              <p className="deliveryNum">+0원</p>
+              <div className="paymentBorderTwo">
+                <div className="pay">
+                  <p className="payMoney">결제예상금액</p>
+                  <p className="lastNum">0원</p>
+                </div>
+              </div>
+              <div className="payButton">
+                <button className="paymentButton">0원 주문하기</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
