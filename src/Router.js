bb880a7f09d4a0ea7fd3ab88cb5271b4f45dbd29
이지원 <manz19@ijiwon-ui-MacBookAir.local>
2023-07-04 18:35:57 +0900

    feature/cart-layout css 수정

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
-// import Nav from './components/Nav/Nav';
+import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import Cart from './pages/Cart';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      {/* <Nav /> */}
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/Cart" element={<Cart />} />
